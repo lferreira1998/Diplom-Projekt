d95defdaa0741438883b4320905d4d94b5e54f4f
Joel Luca Sequeira Ferreira <lferreira@hfg-karlsruhe.de>
2026-05-15 01:12:05 +0200

    Use scrolling dot grid playground wrapper

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -17,7 +17,7 @@ import InASpiral from "./projects/inaspiral/App";
 import RandomlySpatially from "./projects/randomlyspatially/App";
 import New from "./New";
 import Playground from "./Playground";
-import PlaygroundNew from "./PlaygroundNew";
+import PlaygroundNewScrollDots from "./PlaygroundNewScrollDots";
 
 export default function App() {
   return (
@@ -39,8 +39,8 @@ export default function App() {
       <Route path="/aboutnew" element={<AboutNew />} />
       <Route path="/all-tools" element={<AllTools />} />
       <Route path="/playground" element={<Playground />} />
-      <Route path="/playgroundnew" element={<PlaygroundNew />} />
-      <Route path="/playgroundnew1" element={<PlaygroundNew />} />
+      <Route path="/playgroundnew" element={<PlaygroundNewScrollDots />} />
+      <Route path="/playgroundnew1" element={<PlaygroundNewScrollDots />} />
       <Route path="/new" element={<New />} />
     </Routes>
   );
